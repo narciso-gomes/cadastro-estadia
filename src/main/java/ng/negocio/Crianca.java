ad9910a689f7065c287f997bbda96d8e471b37be
narcisogomes <narcisogomes@localhost>
2023-12-08 09:55:25 -0300

    Doc

--- src/main/java/ng/negocio/Crianca.java
+++ src/main/java/ng/negocio/Crianca.java
@@ -5,18 +5,31 @@
 package ng.negocio;
 
 /**
- *
+ * Representa uma criança com informações como nome, idade, sexo e responsável.
+ * 
  * @author narcisogomes
+ * @version 1.0
  */
 public class Crianca extends Pessoa {
 
     private Responsavel responsavel;
     private String sexo;
 
+    /**
+     * Obtém o responsável da criança.
+     * 
+     * @return O responsável da criança.
+     */
     public Responsavel getResponsavel() {
         return this.responsavel;
     }
 
+    /**
+     * Define o responsável da criança.
+     * 
+     * @param responsavel O novo responsável da criança.
+     * @throws IllegalArgumentException Se o responsável fornecido for nulo.
+     */
     public void setResponsavel(Responsavel responsavel) {
         if (responsavel == null) {
             throw new IllegalArgumentException("Responsavel não pode ser nulo");
@@ -24,10 +37,21 @@ public class Crianca extends Pessoa {
         this.responsavel = responsavel;
     }
 
+    /**
+     * Obtém o sexo da criança.
+     * 
+     * @return O sexo da criança.
+     */
     public String getSexo() {
         return sexo;
     }
 
+    /**
+     * Define o sexo da criança.
+     * 
+     * @param sexo O novo sexo da criança.
+     * @throws IllegalArgumentException Se o sexo fornecido for nulo ou vazio.
+     */
     public void setSexo(String sexo) {
         if (sexo == null || sexo.trim().isEmpty()) {
             throw new IllegalArgumentException("Sexo não pode ser nulo ou vazio");
@@ -35,10 +59,21 @@ public class Crianca extends Pessoa {
         this.sexo = sexo;
     }
 
+    /**
+     * Obtém o nome da criança.
+     * 
+     * @return O nome da criança.
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Define o nome da criança.
+     * 
+     * @param nome O novo nome da criança.
+     * @throws IllegalArgumentException Se o nome fornecido for nulo ou vazio.
+     */
     public void setNome(String nome) {
         if (nome == null || nome.trim().isEmpty()) {
             throw new IllegalArgumentException("Nome não pode ser nulo ou vazio");
@@ -46,12 +81,23 @@ public class Crianca extends Pessoa {
         this.nome = nome;
     }
 
+    /**
+     * Obtém a idade da criança.
+     * 
+     * @return A idade da criança.
+     */
     public Integer getIdade() {
         return idade;
     }
 
+    /**
+     * Define a idade da criança.
+     * 
+     * @param idade A nova idade da criança.
+     * @throws IllegalArgumentException Se a idade fornecida estiver fora do intervalo de 0 a 10 anos.
+     */
     public void setIdade(Integer idade) {
-        if (idade > 10 || idade < 0) {
+        if (idade < 0 || idade > 10) {
             throw new IllegalArgumentException("Criança precisa ter de 0 a 10 anos para poder brincar.");
         }
         this.idade = idade;
--- src/main/java/ng/negocio/Estadia.java
+++ src/main/java/ng/negocio/Estadia.java
@@ -5,7 +5,8 @@
 package ng.negocio;
 
 /**
- *
+ * Representa uma estadia de uma criança e controla o tempo utilizado e o custo associado.
+ * 
  * @author narcisogomes
  */
 public class Estadia {
@@ -14,30 +15,59 @@ public class Estadia {
     private double total;
     private final Double VALOR_POR_MINUTO = 1.50;
 
+    /**
+     * Obtém a criança associada a esta estadia.
+     * 
+     * @return A criança associada a esta estadia.
+     */
     public Crianca getCrianca() {
         return crianca;
     }
 
+    /**
+     * Define a criança associada a esta estadia.
+     * 
+     * @param crianca A criança a ser associada a esta estadia.
+     * @throws IllegalArgumentException Se a criança fornecida for nula.
+     */
     public void setCrianca(Crianca crianca) {
         if (crianca == null) {
-            throw new IllegalArgumentException("Responsavel não pode ser nulo");
+            throw new IllegalArgumentException("Criança não pode ser nula");
         }
         this.crianca = crianca;
     }
 
+    /**
+     * Obtém o tempo utilizado nesta estadia.
+     * 
+     * @return O tempo utilizado nesta estadia.
+     */
     public Integer getTempoUtilizado() {
         return tempoUtilizado;
     }
 
+    /**
+     * Define o tempo utilizado nesta estadia e calcula o custo total.
+     * 
+     * @param tempoUtilizado O tempo utilizado nesta estadia.
+     */
     public void setTempoUtilizado(Integer tempoUtilizado) {
         this.tempoUtilizado = tempoUtilizado;
         setTotal();
     }
 
+    /**
+     * Obtém o custo total desta estadia.
+     * 
+     * @return O custo total desta estadia.
+     */
     public Double getTotal() {
         return total;
     }
 
+    /**
+     * Calcula o custo total com base no tempo utilizado, aplicando descontos conforme necessário.
+     */
     private void setTotal() {
         double valorSemDesconto = VALOR_POR_MINUTO * tempoUtilizado;
         double desconto = 0.0;
